07457dadba7ce95194aba38d62d379a47c88199c
rafaa1999 <raferhouma10@gmail.com>
2023-07-19 13:59:33 +0100

    setup the project

---
+++ src/test/java/com/rafaa/springblog/SpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaa.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
